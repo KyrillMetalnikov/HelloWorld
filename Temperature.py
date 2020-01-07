eb7c1f798e4e9c3243926b3c04f36e8e89d91c0e
Tokeybee <tokeybees@gmail.com>
2020-01-07 13:41:45 -0800

    First attempt

---
+++ Temperature.py
@@ -0,0 +1,9 @@
+celsius = 10
+fahrenheit = celsius * 9/5 + 32
+print("{} degrees celsius is equal to {} degrees fahrenheit!".format(celsius, fahrenheit))
+print("Roses are red")
+print("Violets are blue")
+print("Sugar is sweet")
+print("But I have commitment issues")
+print("So I'd rather just be friends")
+print("At this point in our relationship")
